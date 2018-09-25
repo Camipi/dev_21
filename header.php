88f2d1fa74bf7b6775b7b45b2fa9a3113e46e853
Camila <camila@MacBook-Pro-de-Usuario.local>
2018-09-25 12:21:16 -0300

    Agrega meta tags/html en header.php

--- header.php
+++ header.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<meta charset="UTF-8"/>
 	<meta charset="<?php bloginfo('charset') ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="<?php get_template_directory_uri(); ?>/css/app.css"/>
+	<title>Dev_21</title>
 	<?php get_template_part('_includes/iOS', 'icons') ?>
 	<?php wp_head() ?>
 </head>
@@ -12,3 +15,49 @@
 <?php if ( has_nav_menu( 'header-menu' ) ) { ?>
 	<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'header_menu' ) ); ?>
 <?php } ?>
+
+<header>
+		<!--Navbar-->  
+		<nav class="navbar navbar-expand-lg fixed-top">
+			<a class="navbar-brand" href="index.html">
+				<img class="navbar-brand" src="<?php echo get_bloginfo('template_url') ?>/images/logo_dev21.svg" width="120" height="auto" alt="logo dev21">
+  			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    	<span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+			</button>
+
+			<div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+			    <ul class="nav">
+				  <li class="nav-item">
+				    <a class="nav-link active" href="#dev21">Dev21</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="servicios.html">Servicios</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="portafolio.html">Portafolio</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="blog.html">Blog</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="#contacto">Contacto</a>
+				  </li>				  
+				</ul>
+			</div>
+
+			<div class="collapse navbar-collapse justify-content-end redes" id="navbarSupportedContent">
+			    <ul class="nav">
+				  <li class="nav-item">
+				    <a class="nav-link active" href="#"><i class="fab fa-facebook-square"></i></a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="#"><i class="fab fa-twitter-square"></i></a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="#"><i class="fab fa-linkedin"></i></a>
+				  </li>				  		  
+				</ul>
+			</div>
+		</nav>
+	</header>
