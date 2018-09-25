d3cbead1a05c88d7fd2167ed6b069fdc2db34e52
Camila <camila@MacBook-Pro-de-Usuario.local>
2018-09-25 12:24:52 -0300

    Agrega html a footer.php

--- footer.php
+++ footer.php
@@ -3,5 +3,47 @@
 <?php } ?>
 
 <?php wp_footer() ?>
+
+<!-- Footer -->
+	<div class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4 justify-content-center">
+					<img src="img/robot-head.svg" alt="">
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+		</div>
+		<ul class="nav justify-content-center">
+		  <li class="nav-item">
+		    <a class="nav-link active" href="#dev21">Dev21</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="servicios.html">Servicios</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="portafolio.html">Portafolio</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="blog.html">Blog</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#contacto">Contacto</a>
+		  </li>				  
+		</ul>
+		<ul class="nav justify-content-center redes-foo">
+		  <li class="nav-item">
+		    <a class="nav-link active" href="#"><i class="fab fa-facebook-square"></i></a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#"><i class="fab fa-twitter-square"></i></a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#"><i class="fab fa-linkedin"></i></a>
+		  </li>				  		  
+		</ul>
+		<p>Dev_21 2018 <i class="far fa-copyright"></i></p>
+	</div>
 </body>
 </html>
