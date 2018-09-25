f6f696608023d69c28adca87751c276f695524c8
Camila <camila@MacBook-Pro-de-Usuario.local>
2018-09-25 12:40:48 -0300

    Agrega html a front-page.php

---
+++ front-page.php
@@ -0,0 +1,157 @@
+<?php get_header(); ?>
+
+	<!-- Hero -->
+	<section class="hero">
+		<div class="row country">
+				<div class="col-md-8"></div>
+				<div class="col-md-4">
+					<span id="country"></span>
+				</div>
+			</div>
+		<div class="row">
+			<div class="container">
+				<div class="col-md-12">
+					<h1><strong>Bienvenido a<br> Dev_21</strong></h1>
+					<h2>Lorem Ipsum is simply<br> dummy text of the<br> printing.</h2>
+					<p><a href="#dev21"><i class="far fa-arrow-alt-circle-down"></i></a></p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Dev21 -->
+	<div id="dev21" class="dev21">
+		<div class="container">
+			<h1>Dev_21</h1>
+			<p class="dev21-par1">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+			<p class="dev21-par2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.<br>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+		</div>
+	</div>
+
+	<!-- Servicios -->
+	<div id="servicios" class="servicios">
+		<h2>Servicios</h2>
+		<p class="serv-sub">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<img class="img-fluid" src="img/icon1.svg" alt="icono de servicio">
+					<h4>SERVICIO</h4>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+				
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<img class="img-fluid" src="img/icon2.svg" alt="icono de servicio">
+					<h4>SERVICIO</h4>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+				
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<img class="img-fluid" src="img/icon3.svg" alt="icono de servicio">
+					<h4>SERVICIO</h4>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+			</div>
+			<hr/>
+			<div class="row">
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<img class="img-fluid" src="img/icon1.svg" alt="icono de servicio">
+					<h4>SERVICIO</h4>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<img class="img-fluid" src="img/icon2.svg" alt="icono de servicio">
+					<h4>SERVICIO</h4>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<img class="img-fluid" src="img/icon3.svg" alt="icono de servicio">
+					<h4>SERVICIO</h4>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Portafolio -->
+	<div id="portafolio" class="portafolio">
+		<h2>Portafolio</h2>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+					<img src="img/banner.svg" class="img-fluid" alt="">
+					<img src="img/banner.svg" class="img-fluid" alt="">
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+					<img src="img/banner.svg" class="img-fluid" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+					<img src="img/banner.svg" class="img-fluid" alt="">
+					<img src="img/banner.svg" class="img-fluid" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Contacto-->
+	<div id="contacto" class="contacto">
+		<h2>Contacto</h2>
+		<p class="cont-sub">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		<div id="map"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 cont-mail">
+					<p><i class="fas fa-envelope"></i> contacto@dev21.cl</p>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 cont-dir justify-content-center">
+					<p>
+						<i class="fas fa-map-marker-alt"></i> Selecciona la oficina que deseas visitar
+						<select class="custom-select">
+							<option selected>Oficinas</option>
+							<option value="1">Manuel Rodríguez 867. Ofic. 708</option>
+							<option value="2">Lord Cochrane 635. Ofic. 1409A</option>
+						</select>
+						</p>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 cont-telf">
+					<p><i class="fas fa-phone-volume"></i> +56 9 7977 3457</p>
+				</div>
+			</div>
+		</div>
+		<div id="cotizacion">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+						<h4>Pide tu cotización</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta voluptatem, tenetur eum tempore minus libero voluptates eos doloremque.</p>
+					</div>
+					<div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+						<form>
+							 <div class="form-row">
+							    <div class="form-group col-md-6">
+							    	<label for="inputEmail4">Nombre</label>
+							    	<input type="email" class="form-control" id="inputEmail4" placeholder="Ingresa tu nombre">
+							    </div>
+							    <div class="form-group col-md-6">
+							    	<label for="inputPassword4">Email</label>
+							    	<input type="password" class="form-control" id="inputPassword4" placeholder="Ingresa tu email">
+							    </div>
+							</div>
+						  	<div class="form-group">
+						    	<label for="exampleFormControlTextarea1">Mensaje</label>
+						    	<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Ingresa tu mensaje"></textarea>
+						  	</div>
+							<button type="submit" class="btn btn-primary">Enviar</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+<?php get_footer(); ?>
