1a43a12123bcbfd47b38a2d8e307bcd255feb998
Camila <camila@MacBook-Pro-de-Usuario.local>
2018-09-26 13:02:30 -0300

    Agrega html al page template portafolio

---
+++ page-portafolio.php
@@ -0,0 +1,112 @@
+<?php get_header() ?>
+	<<!-- ?php the_post() ?>
+	
+	<?php the_post_thumbnail() ?>
+	<?php the_title() ?>
+	<?php the_content(); ?> -->
+
+	<!-- Hero -->
+	<section class="hero-port">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+					<h1><strong>Nuestro<br> Portafolio</strong></h1>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+					<p id="flechita"><i class="far fa-arrow-alt-circle-right"></i></p>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Proyectos -->
+	<div class="proyectos-port">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<h5>Proyecto</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore consequatur cumque.</p>
+					<h6>Cliente</h6>
+					<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam praesentium similique aspernatur aut velit, totam perspiciatis nostrum quibusdam at culpa ipsam pariatur sed, perferendis. Voluptatibus culpa sint sed odio enim!</p>
+				</div>
+				<div class="col-md-8"><img src="img/banner.svg" alt=""></div>
+			</div>
+			<div class="gal-img-1">
+				<div class="row">
+					<div class="col-md-3">
+						<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+					</div>
+					<div class="col-md-6">
+						<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+					</div>
+					<div class="col-md-3">
+						<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+					</div>
+				</div>
+			</div>
+			<hr/>
+			<div class="row">
+				<div class="col-md-8">
+					<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+				</div>
+				<div class="col-md-4">
+					<h5>Proyecto</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore consequatur cumque.</p>
+					<h6>Cliente</h6>
+					<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam praesentium similique aspernatur aut velit, totam perspiciatis nostrum quibusdam at culpa ipsam pariatur sed, perferendis. Voluptatibus culpa sint sed odio enim!</p>
+				</div>
+			</div>
+			<div class="gal-img-2">
+				<div class="row">
+					<div class="col-md-3">
+						<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+					</div>
+					<div class="col-md-6">
+						<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+					</div>
+					<div class="col-md-3">
+						<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+					</div>
+				</div>
+			</div>
+			<hr/>
+			<div class="row">
+				<div class="col-md-4">
+					<h5>Proyecto</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore consequatur cumque.</p>
+					<h6>Cliente</h6>
+					<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam praesentium similique aspernatur aut velit, totam perspiciatis nostrum quibusdam at culpa ipsam pariatur sed, perferendis. Voluptatibus culpa sint sed odio enim!</p>
+				</div>
+				<div class="col-md-8">
+					<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+				</div>
+			</div>
+			<div class="gal-img-3">
+				<div class="row">
+					<div class="col-md-3">
+						<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+					</div>
+					<div class="col-md-6">
+						<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+					</div>
+					<div class="col-md-3">
+						<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php get_footer() ?>
