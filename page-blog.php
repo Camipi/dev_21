3153351f04f2f3665ec806d25ef21483a9b52904
Camila <camila@MacBook-Pro-de-Usuario.local>
2018-09-26 13:08:55 -0300

    Agrega html al page template blog

---
+++ page-blog.php
@@ -0,0 +1,159 @@
+<?php get_header() ?>
+<!-- 	<?php the_post() ?>
+	
+	<?php the_post_thumbnail() ?>
+	<?php the_title() ?>
+	<?php the_content(); ?> -->
+
+	<!-- Hero -->
+	<section class="hero-blog">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+					<h1><strong>Dev_21<br>Noticias</strong></h1>
+				</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+					<h5><strong>Noticias más recientes</strong></h5>
+
+					<div class="row">
+						<div class="col-md-5">
+							<h6>Titulo de la noticia</h6>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti a tempora ratione ipsum unde.</p>
+						</div>
+						<div class="col-md-7">
+							<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-5">
+							<h6>Titulo de la noticia</h6>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti a tempora ratione ipsum unde.</p>
+						</div>
+						<div class="col-md-7">
+							<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- Noticias -->
+	<div class="noticias-blog">
+		<div class="container">
+
+			<!-- Noticia 1 -->
+			<div class="noticia-blog">
+				<div class="row">
+					<div class="col-md-12">
+						<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+						<div class="row">
+							<div class="col-md-8">
+								<h3 class="noti-titulo">Titulo</h3>
+							</div>
+							<div class="col-md-4">
+								<p class="izq noti-fecha">23 Septiembre 2018</p>
+							</div>
+						</div>
+						<p class="noti-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, repellendus, quibusdam! Nemo molestias atque ad, illo beatae in ab cupiditate, praesentium harum maiores dolorem temporibus assumenda possimus culpa libero unde. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, minima. Obcaecati reiciendis, illum sed libero culpa, soluta fuga sit quidem minus amet distinctio. Expedita, ipsa. Ut asperiores dolorum atque laudantium!
+						</p>
+						<div class="row">
+							<div class="col-md-8">
+								<p><strong>Tags:</strong> etiquetas / etiquetas / etiquetas</p>
+							</div>
+							<div class="col-md-4">
+								<p class="izq"><strong>Autor:</strong> Fulanito de tal</p>
+							</div>
+						</div>				
+					</div>
+				</div>
+				<hr/>
+			</div>
+
+			<!-- Noticia 2 -->
+			<div class="noticia-blog">
+				<div class="row">
+					<div class="col-md-12">
+						<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+						<div class="row">
+							<div class="col-md-8">
+								<h3 class="noti-titulo">Titulo</h3>
+							</div>
+							<div class="col-md-4">
+								<p class="izq noti-fecha">23 Septiembre 2018</p>
+							</div>
+						</div>
+						<p class="noti-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, repellendus, quibusdam! Nemo molestias atque ad, illo beatae in ab cupiditate, praesentium harum maiores dolorem temporibus assumenda possimus culpa libero unde. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, minima. Obcaecati reiciendis, illum sed libero culpa, soluta fuga sit quidem minus amet distinctio. Expedita, ipsa. Ut asperiores dolorum atque laudantium!
+						</p>
+						<div class="row">
+							<div class="col-md-8">
+								<p><strong>Tags:</strong> etiquetas / etiquetas / etiquetas</p>
+							</div>
+							<div class="col-md-4">
+								<p class="izq"><strong>Autor:</strong> Fulanito de tal</p>
+							</div>
+						</div>				
+					</div>
+				</div>
+				<hr/>
+			</div>
+			
+			<!-- Noticia 3 -->
+			<div class="noticia-blog">
+				<div class="row">
+					<div class="col-md-12">
+						<img class="img-fluid" src="<?php echo get_bloginfo('template_url') ?>/assets/images/banner.svg" alt="icono de servicio">
+						<div class="row">
+							<div class="col-md-8">
+								<h3 class="noti-titulo">Titulo</h3>
+							</div>
+							<div class="col-md-4">
+								<p class="izq noti-fecha">23 Septiembre 2018</p>
+							</div>
+						</div>
+						<p class="noti-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, repellendus, quibusdam! Nemo molestias atque ad, illo beatae in ab cupiditate, praesentium harum maiores dolorem temporibus assumenda possimus culpa libero unde. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, minima. Obcaecati reiciendis, illum sed libero culpa, soluta fuga sit quidem minus amet distinctio. Expedita, ipsa. Ut asperiores dolorum atque laudantium!
+						</p>
+						<div class="row">
+							<div class="col-md-8">
+								<p><strong>Tags:</strong> etiquetas / etiquetas / etiquetas</p>
+							</div>
+							<div class="col-md-4">
+								<p class="izq"><strong>Autor:</strong> Fulanito de tal</p>
+							</div>
+						</div>				
+					</div>
+				</div>
+			</div>
+
+			<!-- Flechas -->
+			<div class="flechas-blog">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 col-md-2">
+							<a href="#"><i class="far fa-arrow-alt-circle-left"></i></a>
+						</div>
+						<div class="col-sm-12 col-md-8">
+							<div class="paginacion-blog">
+								<ul class="pagination pagination-sm">
+							    	<li class="page-item disabled">
+							      		<a class="page-link" href="#" tabindex="-1">1</a>
+							    	</li>
+							    	<li class="page-item"><a class="page-link" href="#">2</a></li>
+							    	<li class="page-item"><a class="page-link" href="#">3</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-sm-12 col-md-2">
+							<a href="#"><i class="far fa-arrow-alt-circle-right"></i></a>
+						</div>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php get_footer() ?>
